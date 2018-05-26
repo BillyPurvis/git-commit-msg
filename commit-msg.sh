29f6da76996d932023579fcceae1ba9c8b9c8120
Billy Purvis <purvisbilly@outlook.com>
2018-05-26 11:03:52 +0100

    Updates err message api letter casing

--- commit-msg.sh
+++ commit-msg.sh
@@ -10,6 +10,6 @@ file=`cat $1`
 
 # Test commit message conforms to regex requirements. 
 if ! [[ $file =~ $regex ]]; then
-    echo "${RED} Commit messages must be prefixed with singular or plural fix, change, merge, add. \n Example 'Adds - new api connection input to dash'"    
+    echo "${RED} Commit messages must be prefixed with singular or plural fix, change, merge, add. \n Example 'Adds - new API connection input to dash'"    
     exit 1;    
 fi
