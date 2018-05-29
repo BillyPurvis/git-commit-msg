3e60fdb980a25dfa30708f6178b49a8e14b801b4
BillyPurvis <purvisbilly@outlook.com>
2018-05-29 13:32:47 +0100

    changes script to update incorrect msg

--- commit-msg.sh
+++ commit-msg.sh
@@ -6,10 +6,12 @@ RED='\033[0;31m'
 regex='^(fix(es)?|change(s)?|merge(s)?|update(s)?|add(s)?remove(s)?)[[:space:]].+[a-zA-Z?!${}()@].{5,}'
 
 # Get commit Message
-file=`cat $1` 
+#file=`cat $1` 
 
 # Test commit message conforms to regex requirements. 
-if ! [[ $file =~ $regex ]]; then
-    echo "${RED} Commit messages must be prefixed with singular or plural fix, change, merge, add. \n Example 'Adds - new API connection input to dash'"    
-    exit 1;    
+if ! [[ $1 =~ $regex ]]; then
+    echo "${RED} Commit messages must be prefixed with singular or plural fix, change, merge, add. \n Example 'Adds new API connection input to dash' and at least 5 characters long"    
+    exit 1; 
+    else 
+    echo "Success"  
 fi
